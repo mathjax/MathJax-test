4e2e9fc3b4576242a63a3fc8f55a29838b0a4b30
Frédéric Wang <fred.wang@free.fr>
2011-03-11 17:13:29 +0100

    add reftest for issue 36 ; implement tree reftests for MSIE

--- header.js
+++ header.js
@@ -1,31 +1,31 @@
-/* -*- Mode: Java; tab-width: 2; indent-tabs-mode:nil; -*- */
+/* -*- Mode: Javascript; tab-width: 2; indent-tabs-mode:nil; -*- */
 /* vim: set ts=2 et sw=2 tw=80: */
 /* ***** BEGIN LICENSE BLOCK *****
-/* Version: Apache License 2.0
- *
- * Copyright (c) 2011 Design Science, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Contributor(s):
- *   Frederic Wang <fred.wang@free.fr> (original author)
- *
- * ***** END LICENSE BLOCK ***** */
+   /* Version: Apache License 2.0
+   *
+   * Copyright (c) 2011 Design Science, Inc.
+   *
+   * Licensed under the Apache License, Version 2.0 (the "License");
+   * you may not use this file except in compliance with the License.
+   * You may obtain a copy of the License at
+   *
+   * http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
+   *
+   * Contributor(s):
+   *   Frederic Wang <fred.wang@free.fr> (original author)
+   *
+   * ***** END LICENSE BLOCK ***** */
 
 function getDefaultMathJaxPath()
 {
-src = location.protocol + "//" + location.host + location.pathname;
-return src.substring(0, src.search(/MathJax-test/)) + "MathJax/";
+    src = location.protocol + "//" + location.host + location.pathname;
+    return src.substring(0, src.search(/MathJax-test/)) + "MathJax/";
 }
 
 var gMathJaxPath = getDefaultMathJaxPath();
@@ -34,82 +34,82 @@ var gFonts = "STIX";
 
 function parseQueryString()
 {
-  var query = location.search.substring(1);
-  var pairs = query.split("&");
-  for(var i = 0; i < pairs.length; i++) {
-    var pos = pairs[i].indexOf("=");
-    if (pos == -1) {
-      continue;
+    var query = location.search.substring(1);
+    var pairs = query.split("&");
+    for(var i = 0; i < pairs.length; i++) {
+        var pos = pairs[i].indexOf("=");
+        if (pos == -1) {
+            continue;
+        }
+        var paramname = pairs[i].substring(0, pos);
+        var paramvalue = pairs[i].substring(pos + 1);
+        if (paramname == "mathJaxPath") {
+            gMathJaxPath = paramvalue;
+        }
+        if (paramname == "fonts") {
+            gFonts = paramvalue;
+        }
+        if (paramname == "nativeMathML") {
+            gNativeMathML = (paramvalue == "true");
+        }
     }
-    var paramname = pairs[i].substring(0, pos);
-    var paramvalue = pairs[i].substring(pos + 1);
-    if (paramname == "mathJaxPath") {
-      gMathJaxPath = paramvalue;
-    }
-    if (paramname == "fonts") {
-      gFonts = paramvalue;
-    }
-    if (paramname == "nativeMathML") {
-      gNativeMathML = (paramvalue == "true");
-    }
-  }
 }
 
 function startMathJax()
 {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  src = location.href;
-  script.src = gMathJaxPath + "MathJax.js";
-  var config =
-    'MathJax.Hub.Config({' +
-    'messageStyle: "none",' +
-    // , "TeX/AMSmath.js", "TeX/AMSsymbols.js"
-    'extensions: ["tex2jax.js",  "mml2jax.js"],';
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    src = location.href;
+    script.src = gMathJaxPath + "MathJax.js";
+    var config =
+        'MathJax.Hub.Config({' +
+        'messageStyle: "none",' +
+        // , "TeX/AMSmath.js", "TeX/AMSsymbols.js"
+        'extensions: ["tex2jax.js",  "mml2jax.js"],';
 
-  config +=
+    config +=
     'jax: ["input/TeX", "input/MathML", ' +
-       (gNativeMathML ? '"output/NativeMML"' : '"output/HTML-CSS"') + '],';
+        (gNativeMathML ? '"output/NativeMML"' : '"output/HTML-CSS"') + '],';
 
-  if (gFonts == "ImageTeX") {
-    fonts = "";
-  } else {
-    fonts = '"' + gFonts + '"';
-  }
-  config +=
+    if (gFonts == "ImageTeX") {
+        fonts = "";
+    } else {
+        fonts = '"' + gFonts + '"';
+    }
+    config +=
     '"HTML-CSS": {' +
-    '  availableFonts: [' + fonts + '], preferredFont: null, webFont: null' +
-    '},';
+        '  availableFonts: [' + fonts + '],' +
+        'preferredFont: null, webFont: null' +
+        '},';
 
-  config +=
+    config +=
     'TeX: {' +
-    '  extensions: ["AMSmath.js", "AMSsymbols.js"]' +
-    '}';
+        '  extensions: ["AMSmath.js", "AMSsymbols.js"]' +
+        '}';
 
-  config +=
+    config +=
     '});'
 
-  config +=
+    config +=
     'MathJax.Hub.Startup.onload();' +
-    'MathJax.Hub.Queue(function () {' +
-    'document.documentElement.className = "";' +
-    '});';
+        'MathJax.Hub.Queue(function () {' +
+        'document.documentElement.className = "";' +
+        '});';
 
-  if (window.opera) {
-    script.innerHTML = config;
-  } else {
-    script.text = config;
-  }
+    if (window.opera) {
+        script.innerHTML = config;
+    } else {
+        script.text = config;
+    }
 
-  document.getElementsByTagName("head")[0].appendChild(script);
+    document.getElementsByTagName("head")[0].appendChild(script);
 }
 
 parseQueryString();
 // XXXfred: Reftests executed with Mozilla's runreftest.py should really
 // call startMathJax when the MozReftestInvalidate event happens. 
 if (window.addEventListener) {
-  window.addEventListener("load", startMathJax, false);
+    window.addEventListener("load", startMathJax, false);
 } else if (window.attachEvent){
-  window.attachEvent("onload", startMathJax);
+    window.attachEvent("onload", startMathJax);
 }
-
--- treeReftests.js
+++ treeReftests.js
@@ -1,48 +1,132 @@
-/* -*- Mode: Java; tab-width: 2; indent-tabs-mode:nil; -*- */
+/* -*- Mode: Javascript; tab-width: 2; indent-tabs-mode:nil; -*- */
 /* vim: set ts=2 et sw=2 tw=80: */
 /* ***** BEGIN LICENSE BLOCK *****
-/* Version: Apache License 2.0
- *
- * Copyright (c) 2011 Design Science, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Contributor(s):
- *   Frederic Wang <fred.wang@free.fr> (original author)
- *
- * ***** END LICENSE BLOCK ***** */
+   /* Version: Apache License 2.0
+   *
+   * Copyright (c) 2011 Design Science, Inc.
+   *
+   * Licensed under the Apache License, Version 2.0 (the "License");
+   * you may not use this file except in compliance with the License.
+   * You may obtain a copy of the License at
+   *
+   * http://www.apache.org/licenses/LICENSE-2.0
+   *
+   * Unless required by applicable law or agreed to in writing, software
+   * distributed under the License is distributed on an "AS IS" BASIS,
+   * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   * See the License for the specific language governing permissions and
+   * limitations under the License.
+   *
+   * Contributor(s):
+   *   Frederic Wang <fred.wang@free.fr> (original author)
+   *
+   * ***** END LICENSE BLOCK ***** */
 
 function serialize(aNode)
 {
-  // XXXfred: Internet Explorer only supports XMLSerializer since version 9
-  source = (new XMLSerializer()).serializeToString(aNode);
-  
-  // add linebreaks to help diffing source
-  source = source.replace(/>(?!<)/g, ">\n")
-  source = source.replace(/</g, "\n<")
+    try {
+	source = (new XMLSerializer()).serializeToString(aNode);
+    } catch(e) {
+	if (e instanceof TypeError) {
+            // XXXfred: Internet Explorer only supports XMLSerializer since
+            // version 9
+            source = serialize2(aNode);
+	} else {
+            throw e;
+	}
+    }
 
-  return source;
+    // add linebreaks to help diffing source
+    source = source.replace(/>(?!<)/g, ">\n");
+    source = source.replace(/</g, "\n<");
+
+    return source;
+}
+
+function serialize2(aNode)
+{
+    // a basic serializer for browsers that do not support XMLSerializer
+    // it is not claimed to be complete
+
+    var s = "";
+
+    switch(aNode.nodeType)
+    {
+    case Node.TEXT_NODE:
+        s += aNode.data;
+        break;
+        
+    case Node.COMMENT_NODE:
+        s += "<!--"
+        s += aNode.value;
+        s += "-->"
+        break;
+
+    case Node.CDATA_SECTION_NODE:
+        s += "<![CDATA["
+        s += aNode.value;
+        s += "]]>"
+        break;
+
+    case Node.ATTRIBUTE_NODE:
+        s += aNode.name;
+        s += '=';
+        s += '"' + aNode.value + '"';
+        break;
+
+    case Node.ELEMENT_NODE:
+        s += "<";
+        s += aNode.tagName;
+        var attributes = aNode.attributes;
+        for (var i = 0; i < attributes.length; i++) {
+            s += " " + serialize2(attributes[i]);
+        }
+        s += ">";
+        var children = aNode.childNodes;
+        for (var i = 0; i < children.length; i++) {
+            s += serialize2(children[i]);
+        }
+        s += "</";
+        s += aNode.tagName;
+        s += ">"
+        break;
+
+    default:
+        throw "serialization error";
+        break;
+    }
+
+    return s;
 }
 
 function getMathJaxSource(aNode, aClassName)
 {
-   // XXXfred not supported by Internet Explorer?
-   divs = aNode.getElementsByClassName(aClassName);
-   return serialize(divs[0]);
+    try {
+	divs = aNode.getElementsByClassName(aClassName);
+	return serialize(divs[0]);
+    } catch(e) {
+	if (e instanceof TypeError) {
+	    // XXXfred Internet Explorer lacks support for
+	    // getElementsByClassName)
+	    divs = aNode.getElementsByTagName("div");
+	    for (i = 0; i < divs.length; i++) {
+		if (divs[i].className == aClassName) {
+		    return serialize(divs[i]);
+		}
+	    }
+	    throw "MathJax_MathML not found ";
+	} else {
+	    throw e;
+	}
+    }
 }
 
 function getMathJaxSourceMathML()
 {
-  node = document.getElementById("reftest-element");
-  return getMathJaxSource(node, "MathJax_MathML");
+    node = document.getElementById("reftest-element");
+    if (!node) {
+	throw "reftest-element not found";
+    }
+
+    return getMathJaxSource(node, "MathJax_MathML");
 }
