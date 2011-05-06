79a6c9336f5603202bfaffc5009727eb1ba5773a
Frédéric Wang <fred.wang@free.fr>
2011-05-06 19:12:24 +0200

    Several improvement to the Python scripts: - improve generation of output files: store the files in a same directory ; execute the Perl script to convert them to HTML ; compress them with gzip - add start/end time of testing instance as well as duration - allow to pass a list of config files to the python script

--- runTestsuite.py
+++ runTestsuite.py
@@ -22,15 +22,20 @@
 #
 # ***** END LICENSE BLOCK *****
 
-from datetime import datetime
+from datetime import datetime, timedelta
 import ConfigParser
+import argparse
+import errno
+import gzip
+import math
+import os
 import platform
 import reftest
 import seleniumMathJax
-import argparse
+import subprocess
+import sys
 import time
 import unittest
-import sys
 
 def getOperatingSystem(aOperatingSystem):
 
@@ -58,7 +63,7 @@ def getBrowserStartCommand(aBrowserPath, aOS, aBrowser):
     
     if aBrowserPath == "auto":
         if startCommand == "*custom":
-            print "Unknown browser"
+            print >> sys.stderr, "Unknown browser"
             return "unknown"
 
         if aOS == "Linux" and aBrowser == "Konqueror":
@@ -68,19 +73,14 @@ def getBrowserStartCommand(aBrowserPath, aOS, aBrowser):
     
     return startCommand
 
-def getOutputFileName(aOutput, aSelenium):
+def getOutputFileName(aDirectory, aSelenium):
 
-    if aOutput != "auto":
-        return aOutput
-
-    utcdate = datetime.now().isoformat("_")
-    return "results/" + \
+    return \
+        aDirectory + \
         aSelenium.mOperatingSystem + "_" + \
         aSelenium.mBrowser + "_" + \
         aSelenium.mBrowserMode + "_" + \
-        aSelenium.mFont + "_" + \
-        utcdate + \
-        ".txt"
+        aSelenium.mFont
 
 def printInfo(aString):
   print "REFTEST INFO | " + aString
@@ -90,116 +90,159 @@ def boolToString(aBoolean):
         return "true"
     return "false"
 
+def gzipFile(aFile):
+    f_in = open(aFile, "rb")
+    f_out = gzip.open(aFile + ".gz", "wb")
+    f_out.writelines(f_in)
+    f_out.close()
+    f_in.close()
+    os.remove(aFile)
+
 if __name__ == "__main__":
+
+    directory = "results/" + datetime.utcnow().isoformat() + "/"
+    # create the directory (we assume that it does not exist yet, otherwise
+    # this will raise an error)
+    os.makedirs(directory)
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(
         description="A Python script to run MathJax automated tests.")
     parser.add_argument("-c", "--config", nargs='?', default="default.cfg",
-                        help="A file describing the parameters of the \
-automated test instance to run. The default configuration file is default.cfg.")
+                        help="A comma separated list of files describing the \
+parameters of the automated test instance to run. The default configuration \
+file is default.cfg.")
     args = parser.parse_args()
+    configFileList = args.config.split(",")
 
-    # Load configuration file
-    config = ConfigParser.ConfigParser()
-    config.readfp(open(args.config))
+    for configFile in configFileList:
 
-    # framework section
-    section = "framework"
-    host = config.get(section, "host")
-    port = config.getint(section, "port")
-    mathJaxPath = config.get(section, "mathJaxPath")
-    mathJaxTestPath = config.get(section, "mathJaxTestPath")
-    timeOut = config.getint(section, "timeOut")
-    fullScreenMode = config.getboolean(section, "fullScreenMode")
+        if (not os.path.isfile(configFile)):
+            print >> sys.stderr,\
+                "Warning: config file " + configFile + " not found!"
+            continue
+
+        # Load configuration file
+        config = ConfigParser.ConfigParser()
+        config.readfp(open(configFile))
+
+        # framework section
+        section = "framework"
+        host = config.get(section, "host")
+        port = config.getint(section, "port")
+        mathJaxPath = config.get(section, "mathJaxPath")
+        mathJaxTestPath = config.get(section, "mathJaxTestPath")
+        timeOut = config.getint(section, "timeOut")
+        fullScreenMode = config.getboolean(section, "fullScreenMode")
     
-    # platform section
-    section = "platform"
-    operatingSystem = getOperatingSystem(config.get(section, "operatingSystem"))
-    browserList = config.get(section, "browser").split()
-    browserModeList = config.get(section, "browserMode").split()
-    browserPath = config.get(section, "browserPath")
-    fontList = config.get(section, "font").split()
-    nativeMathML = config.getboolean(section, "nativeMathML")
+        # platform section
+        section = "platform"
+        operatingSystem = getOperatingSystem(config.get(section,
+                                                        "operatingSystem"))
+        browserList = config.get(section, "browser").split()
+        browserModeList = config.get(section, "browserMode").split()
+        browserPath = config.get(section, "browserPath")
+        fontList = config.get(section, "font").split()
+        nativeMathML = config.getboolean(section, "nativeMathML")
     
-    # testsuite section
-    section = "testsuite"
-    outputFile = config.get(section, "outputFile")
-    runSlowTests = config.getboolean(section, "runSlowTests")
+        # testsuite section
+        section = "testsuite"
+        runSlowTests = config.getboolean(section, "runSlowTests")
        
-    if (len(browserList) > 1 or len(browserModeList) or len(fontList) > 1):
-        msg =  " can not be specified when launching several instances at the"
-        msg += "same time."
-        if browserPath != "auto":
-            print "browserPath" + msg
-            exit(1)
-        if outputFile != "auto":
-            print "outputFile" + msg
-            exit(1)
+        if ((len(browserList) > 1 or len(browserModeList) or len(fontList) > 1)
+            and browserPath != "auto"):
+            print >> sys.stderr, "Warning: browserPath ignored"
+            browserPath = "auto"
 
-    for browser in browserList:
-        for font in fontList:
-            for browserMode in browserModeList:
+        for browser in browserList:
+            for font in fontList:
+                for browserMode in browserModeList:
 
-                # Browser mode is only relevant for MSIE
-                if not(browser == "MSIE"):
-                    browserMode = "StandardMode"
+                    # Browser mode is only relevant for MSIE
+                    if not(browser == "MSIE"):
+                        browserMode = "StandardMode"
 
-                browserStartCommand = getBrowserStartCommand(
-                    browserPath,
-                    operatingSystem,
-                    browser)
+                        browserStartCommand = getBrowserStartCommand(
+                            browserPath,
+                            operatingSystem,
+                            browser)
 
-                if browserStartCommand != "unknown":
-                        
-                    # Create a Selenium instance
-                    selenium = seleniumMathJax.seleniumMathJax(
-                        host, port, mathJaxPath, mathJaxTestPath,
-                        operatingSystem,
-                        browser,
-                        browserMode,
-                        browserStartCommand, 
-                        font,
-                        nativeMathML,
-                        timeOut,
-                        fullScreenMode)
-                        
-                    # Create the test suite
-                    suite = reftest.reftestSuite(runSlowTests)
-                    suite.addReftests(selenium, "reftest.list")
-                        
-                    # Create the output file
-                    output = getOutputFileName(outputFile, selenium)
-                        
-                    # Run the test suite
-                    selenium.start()
-                        
-                    fp = file(output, "wb")
-                    stdout = sys.stdout
-                    sys.stdout = fp
-                    printInfo("Starting Testing Instance")
-                    printInfo("host=" + str(host))
-                    printInfo("port=" + str(port))
-                    printInfo("mathJaxPath = " + mathJaxPath)
-                    printInfo("mathJaxTestPath = " + mathJaxTestPath)
-                    printInfo("operatingSystem = " + operatingSystem)
-                    printInfo("browser = " + browser)
-                    printInfo("browserMode = " + browserMode)
-                    printInfo("font = " + font)
-                    printInfo("nativeMathML = " +
-                              boolToString(nativeMathML))
-                    printInfo("runSlowTests = " +
-                              boolToString(runSlowTests))
-                    unittest.TextTestRunner(sys.stderr,
-                                            verbosity=2).run(suite)
-                    printInfo("Testing Instance Finished")
-                    sys.stdout = stdout
-                    fp.close()
-                    selenium.stop()
-                    time.sleep(4)
+                        if browserStartCommand != "unknown":
 
-                # end if browserStartCommand
-        # end for font
-    # end browser
+                            # Create a Selenium instance
+                            selenium = seleniumMathJax.seleniumMathJax(
+                                host, port, mathJaxPath, mathJaxTestPath,
+                                operatingSystem,
+                                browser,
+                                browserMode,
+                                browserStartCommand, 
+                                font,
+                                nativeMathML,
+                                timeOut,
+                                fullScreenMode)
+                        
+                            # Create the test suite
+                            suite = reftest.reftestSuite(runSlowTests)
+                            suite.addReftests(selenium, "reftest.list")
+                        
+                            # Create the output file
+                            output = getOutputFileName(directory, selenium)
+                            outputTxt = output + ".txt"
+                            outputHTML= output + ".html"
+                            fp = file(outputTxt, "wb")
+                            stdout = sys.stdout
+                            sys.stdout = fp
+                        
+                            # Run the test suite
+                            startTime = datetime.utcnow()
+                            printInfo("Starting Testing Instance ; " +
+                                      startTime.isoformat())
+                            selenium.start()
+                            printInfo("host=" + str(host))
+                            printInfo("port=" + str(port))
+                            printInfo("mathJaxPath = " + mathJaxPath)
+                            printInfo("mathJaxTestPath = " + mathJaxTestPath)
+                            printInfo("operatingSystem = " + operatingSystem)
+                            printInfo("browser = " + browser)
+                            printInfo("browserMode = " + browserMode)
+                            printInfo("font = " + font)
+                            printInfo("nativeMathML = " +
+                                      boolToString(nativeMathML))
+                            printInfo("runSlowTests = " +
+                                      boolToString(runSlowTests))
+                            unittest.TextTestRunner(sys.stderr,
+                                                    verbosity=2).run(suite)
+                            selenium.stop()
+                            time.sleep(4)
+                            endTime = datetime.utcnow()
+                            deltaTime = endTime - startTime
+                            printInfo("Testing Instance Finished ; " +
+                                      endTime.isoformat())
+                            printInfo("Testing Instance took " + \
+                                      str(math.trunc(
+                                        deltaTime.total_seconds() / 60)) +
+                                      " minute(s) and " + \
+                                          str(deltaTime.seconds) + " second(s)")
+                            sys.stdout = stdout
+                            fp.close()
+
+                            # Execute the Perl script to format the output
+                            print "Formatting the text ouput..."
+                            pipe = subprocess.Popen(
+                                ["./clean-reftest-output.pl", outputTxt],
+                                stdout=subprocess.PIPE)
+                            fp = file(outputHTML, "wb")
+                            print >> fp, pipe.stdout.read()
+                            fp.close()
+
+                            # gzip the outputs
+                            print "Compressing the output files..."
+                            gzipFile(outputTxt)
+                            gzipFile(outputHTML)
+
+                        # end if browserStartCommand
+                # end browserMode
+            # end for font
+        # end browser
 
     exit(0)
